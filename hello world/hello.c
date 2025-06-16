4d317fc6b5df844346bbe47bf1b7c02376fd3b2c
Dopyno <dopyno@yahoo.com>
2025-06-16 20:06:11 +0100

    first program in C

---
+++ hello world/hello.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int account = 0;
+void greetingMsg();
+
+int main(){
+
+greetingMsg();
+
+
+    return 0;
+}
+
+void greetingMsg(){
+    printf("*************************************\n");
+    printf("**********    Welcome!!!    *********\n");
+    printf("*************************************\n");   
+}
