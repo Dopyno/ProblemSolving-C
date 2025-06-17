c019afaa96cb34fc242078b3d0cca99bb9f2707e
Dopyno <dopyno@yahoo.com>
2025-06-17 22:38:30 +0100

    prettier set up

--- hello world/hello.c
+++ hello world/hello.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 #pragma region function prototype
 #define MAX_USERS 10  //* Maximum number of users
 struct User           //*Structure to represent a user
 {
-    int age;
-    char firstName[10];
-    char lastName[10];
-    char email[25];
-    int account;
+  int age;
+  char firstName[10];
+  char lastName[10];
+  char email[25];
+  int account;
 };
 void greetingMsg();
 void saluteUser();
@@ -18,82 +18,86 @@ void printUsers(struct User user);
 int optionStartMenu();
 #pragma endregion
 
-int main(){
+int main() {
+  struct User users[MAX_USERS];
+  int answer, n;
 
-struct User users[MAX_USERS];
-int answer, n;
+  greetingMsg();
 
-greetingMsg();
+  do {
+    int answer = optionStartMenu();
+    switch (answer) {
+      case 1:
+        printf("This is case 1");
+        break;
 
-do{
-int answer = optionStartMenu();
-switch (answer)
-{
-case 1:
-    /* code */
-    break;
+      case 2:
+        printf("This is case 2");
 
-case 2:
+        break;
+      case 0:
+        saluteUser();
 
-    break;
-}
-
-} while (answer != 0);{}
-
-
-// for(int i = 0; i < n; i++){
-// getUserDetail(&users[i]);
-// };
-
-// printf("\n------User Details------\n");
-// for(int i = 0; i < n; i++){
-//     printUsers(users[i]);
-//     };
-saluteUser();
-    return 0;
-}
-
-#pragma region function 
-void greetingMsg(){
-    printf("**************<<<<<<<<<<<<<<<>>>>>>>>>>>>>>*****************\n");
-    printf("**********          Welcome to Silver Bank!!        *********\n");
-    printf("**************<<<<<<<<<<<<<<<>>>>>>>>>>>>>>*****************\n\n\n");   
-}
-
-void getUserDetail(struct User *u){
-   printf("Please enter your age: ");
-    scanf("%d", &u->age);
-    if(u->age > 18){
-        printf("Please enter your first name: ");
-        scanf("%s", u->firstName);
-        printf("Please enter your last name: ");
-        scanf("%s", u->lastName);
-        printf("Please enter your email address: ");
-        scanf("%s", u->email);
-        printf("Please enter the amount you want to deposit today in £: ");
-        scanf("%d", &u->account);
-    }else{
-        printf("Sorry, you are to young at this time to open an account!\nYou must wait till turn 18 years old!\n\n\n");
+        break;
     }
+
+  } while (answer != 0);
+  {
+  }
+
+  // for(int i = 0; i < n; i++){
+  // getUserDetail(&users[i]);
+  // };
+
+  // printf("\n------User Details------\n");
+  // for(int i = 0; i < n; i++){
+  //     printUsers(users[i]);
+  //     };
+
+  return 0;
 }
 
-int optionStartMenu(){
-    int n;
-    printf("Please choose one of the following option or press '0' to exit! \n");
-    printf("1.\t🏦Open acount.\n");
-    printf("2.\t💷My account.\n");
-    printf("0.\tExit.🚀\n");
-    printf("Enter your selection: ");
-    scanf("%d", &n);
-    return n;
+#pragma region function
+void greetingMsg() {
+  printf("**************<<<<<<<<<<<<<<<>>>>>>>>>>>>>>*****************\n");
+  printf("**********          Welcome to Silver Bank!!        *********\n");
+  printf("**************<<<<<<<<<<<<<<<>>>>>>>>>>>>>>*****************\n\n\n");
 }
 
-void printUsers(struct User user){
-printf("Full name: %s %s\n", user.firstName, user.lastName);
-printf("Email: %s\n", user.email);
-printf("Today you have an account of £%d.\n", user.account);
+void getUserDetail(struct User *u) {
+  printf("Please enter your age: ");
+  scanf("%d", &u->age);
+  if (u->age > 18) {
+    printf("Please enter your first name: ");
+    scanf("%s", u->firstName);
+    printf("Please enter your last name: ");
+    scanf("%s", u->lastName);
+    printf("Please enter your email address: ");
+    scanf("%s", u->email);
+    printf("Please enter the amount you want to deposit today in £: ");
+    scanf("%d", &u->account);
+  } else {
+    printf(
+        "Sorry, you are to young at this time to open an account!\nYou must "
+        "wait till turn 18 years old!\n\n\n");
+  }
 }
-void saluteUser(){
-    printf("\nThank you for choosing Silver Bank today!\n");
+
+int optionStartMenu() {
+  int n;
+  printf("Please choose one of the following option or press '0' to exit! \n");
+  printf("1.\t🏦Open acount.\n");
+  printf("2.\t💷My account.\n");
+  printf("0.\tExit.🚀\n");
+  printf("Enter your selection: ");
+  scanf("%d", &n);
+  return n;
 }
+
+void printUsers(struct User user) {
+  printf("Full name: %s %s\n", user.firstName, user.lastName);
+  printf("Email: %s\n", user.email);
+  printf("Today you have an account of £%d.\n", user.account);
+}
+void saluteUser() { printf("\nThank you for choosing Silver Bank today!\n"); }
 #pragma endregion
