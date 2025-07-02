2add3674befa19d3418469c5ed513c913008a9b7
Dopyno <dopyno@yahoo.com>
2025-07-02 14:43:16 +0100

    up

--- coffee machine/coffee.c
+++ coffee machine/coffee.c
@@ -4,17 +4,22 @@
 
 void start();
 void showMenu();
+void askExtras();
+int makingCoffee(int selection);
 
 float total = 0.0, americano = 3.80, espresso = 1.95, latte = 4.40,
       cappuccino = 4.40, hotChocolate = 3.90, tea = 1.95;
 int choice;
+char repeat;
 
 int main() {
+  system("clear");
   start();
   do {
     showMenu();
     printf("Please enter you selection: ");
     scanf("%d", &choice);
+    makingCoffee(choice);
   } while (choice != 0);
 
   return 0;
@@ -39,6 +44,50 @@ void showMenu() {
   printf("Total to pay: £%.2f\n", total);
   printf("========================================\n");
 }
+int makingCoffee(int selection) {
+  switch (selection) {
+    case 1:
+      printf("You chose Americano.\n");
+      askExtras();
+      printf("☕ Making your Americano...\n");
+      total += americano;
+      break;
+    case 2:
+      printf("You chose Espresso.\n");
+      askExtras();
+      printf("☕ Making your Espresso...\n");
+      total += espresso;
+      break;
+    case 3:
+      printf("You chose Latte.\n");
+      askExtras();
+      printf("☕️ Making your Latte...\n");
+      total += latte;
+      break;
+    case 4:
+      printf("You chose Cappuccino.\n");
+      askExtras();
+      printf("☕️ Making your Cappuccino...\n");
+      total += cappuccino;
+      break;
+    case 5:
+      printf("You chose Hot Chocolate.\n");
+      askExtras();
+      printf("☕️ Making your Hot Chocolate...\n");
+      total += hotChocolate;
+      break;
+    case 6:
+      printf("You chose Tea.\n");
+      askExtras();
+      printf("🫖 Making your Tea...\n");
+      total += tea;
+      break;
+    default:
+      printf("Invalid selection, Please make a selection from 1-6");
+      break;
+  }
+}
+
 void askExtras() {
   char milk, sugar;
 
