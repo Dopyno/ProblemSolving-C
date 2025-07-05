5418260b774f0189015216d99066de284af941de
Dopyno <dopyno@yahoo.com>
2025-07-05 11:55:01 +0100

    up

--- First week/second.c
+++ First week/second.c
@@ -6,7 +6,7 @@ int main() {
   char sign;
   float num1, num2;
 
-  printf("Type your expresion: \n");
+  printf("Type your expresion to be calculated: \n");
   scanf("%f%c%f", &num1, &sign, &num2);
 
   switch (sign) {
