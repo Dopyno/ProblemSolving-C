5c6c31ede1b8864e3820978a003cf68a44d8bdd6
Dopyno <dopyno@yahoo.com>
2025-07-05 13:24:35 +0100

    up

--- First week/second.c
+++ First week/second.c
@@ -6,26 +6,27 @@ int main() {
   char sign;
   float num1, num2;
 
-  printf("Type your expresion to be calculated: \n");
-  scanf("%f%c%f", &num1, &sign, &num2);
+  do {
+    printf("\n\nType your expresion to be calculated: \n");
+    scanf("%f %c %f", &num1, &sign, &num2);
 
-  switch (sign) {
-    case '+':
-      printf("%.2f", num1 + num2);
-      break;
-    case '-':
-      printf("%.2f", num1 - num2);
-      break;
-    case '/':
-      printf("%.2f", num1 / num2);
-      break;
-    case '*':
-      printf("%.2f", num1 * num2);
-      break;
-    default:
-      printf("❌Invalid operation!!!\n");
-      break;
-  }
+    switch (sign) {
+      case '+':
+        printf("%.2f", num1 + num2);
+        break;
+      case '-':
+        printf("%.2f", num1 - num2);
+        break;
+      case '/':
+        printf("%.2f", num1 / num2);
+        break;
+      case '*':
+        printf("%.2f", num1 * num2);
+        break;
+      default:
+        printf("\n❌Invalid operation!!!\n");
+    }
+  } while (sign != '+' || sign != '-' || sign != '/' || sign != '*');
 
-  return 0;
+      return 0;
 }
---
+++ First week/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 95, b = 10, rez = 0;
+
+  printf("%d", a / b);
+  printf("\n%d", a % b);
+}
