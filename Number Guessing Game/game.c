8d1f2c6b826916d2b8e74459c9aceb5aee6fb1c6
Dopyno <dopyno@yahoo.com>
2025-07-06 07:39:05 +0100

    game

---
+++ Number Guessing Game/game.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void gameInfo();
+
+int main(void) {
+  int number = 0;
+  char answer;
+  gameInfo();
+
+  do {
+    int userNumber = 0;
+    srand(time(0));
+    int randomNumber = rand() % 10 + 1;
+
+    while (userNumber != randomNumber) {
+      printf("\nPlease enter a number from 1 - 20! \n");
+      scanf("%d", &userNumber);
+
+      if (userNumber > randomNumber)
+        printf("❌Sorry, the number is too high");
+      else if (userNumber < randomNumber)
+        printf("❌Sorry, the number is too low");
+      else
+        printf("Correct, you are the superstar!");
+    }
+
+    printf("\nDo you want to play again? (Y / N)\n");
+    scanf(" %c", &answer);
+  } while (answer == 'y' || answer == 'Y');
+
+  return 0;
+}
+
+void gameInfo() {
+  printf("\n       ***  Welcome  ***\n");
+  printf("*** Let's play Number Guessing Game! ***\n");
+}
