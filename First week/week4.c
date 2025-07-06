e9a313956836e47d6dee443dbbdb1b3d062c41a4
Dopyno <dopyno@yahoo.com>
2025-07-06 13:57:08 +0100

    up

---
+++ First week/week4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  int a, b, results;
+
+  printf("enter a number: ");
+  scanf("%d", &a);
+  printf("enter a second number: ");
+  scanf("%d", &b);
+
+  for (int i = 1; i < a; i++) {
+    for (int j = 1; j < b; j++) {
+      results = a * b;
+      printf("%d * %d = %d\n", i, j, results);
+    }
+    printf("\n");
+  }
+  return 0;
+}
