7e2f9bf0e2fe0124a8c8ad2ae60c83b674d9c04a
Dopyno <dopyno@yahoo.com>
2025-07-12 13:20:27 +0100

    up

--- quizGame/quiz.c
+++ quizGame/quiz.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char question[][50] = {
+char question[][80] = {
     "Q1: What is the correct syntax to output 'Hello World!' in C?\n",
     "Q2: What is the capital of France?\n",
     "Q3: What planet is known as the Red Planet?\n",
@@ -13,18 +13,22 @@ char question[][50] = {
     "Q8: Which header file is required to use printf()?\n",
     "Q9: Which is the largest ocean on Earth?\n",
     "Q10: What color do you get by mixing red and blue?\n"};
-char correctAnswer[][80] = {"\nA. echo(\" Hello, World\");\nB.printf(\"Hello, World\");\nC.cout <<\"Hello, World\";\nD.print(\"Hello, World\")\n",
+char correctAnswer[][256] = {
+    "\nA. echo(\" Hello, World\");\nB.printf(\"Hello, World\");\nC.cout "
+    "<<\"Hello, World\";\nD.print(\"Hello, World\")\n",
 };
 
 void gameInfo();
-void startTheGame(char arr[]);
+void startTheGame(char arr);
+void clearScreen();
 
 int main() {
+    char answ;
+  clearScreen();
   gameInfo();
-
-  for (int i = 0; i <= sizeof(question) / sizeof(question[0]); i++) {
-    printf("%s", question[i]);
-  }
+  printf("\nEnter your answer (A/B/C/D): ");
+  sscanf(" %c", &answ);
+  startTheGame(answ);
 
   return 0;
 }
@@ -33,8 +37,22 @@ void gameInfo() {
   printf("\n      ***  Welcome  ***\n");
   printf("\n*** Let's play Quiz Game! ***\n");
 }
-void startTheGame(char arr[]) {
+void startTheGame(char arr) {
   for (int i = 0; i <= sizeof(question) / sizeof(question[0]); i++) {
     printf("%s", question[i]);
+    if (strcmp(arr, correctAnswer) == 0)
+    {
+      printf("✅ Correct: B\n");
+    }else{
+        printf("❌Wrong answer!!!!");
+    }
+    
   }
+}
+void clearScreen() {
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
 }
