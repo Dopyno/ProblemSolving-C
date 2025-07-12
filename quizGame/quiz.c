051772c62fd04aa96b81506acef981ff5b65cb46
Dopyno <dopyno@yahoo.com>
2025-07-12 19:28:44 +0100

    up

--- quizGame/quiz.c
+++ quizGame/quiz.c
@@ -2,33 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-char question[][80] = {
-    "Q1: What is the correct syntax to output 'Hello World!' in C?\n",
-    "Q2: What is the capital of France?\n",
-    "Q3: What planet is known as the Red Planet?\n",
-    "Q4: Which of the following is a loop in C?\n",
-    "Q5: What is the square root of 81?\n",
-    "Q6: Who was the first President of the United States?\n",
-    "Q7: What does RAM stand for?\n",
-    "Q8: Which header file is required to use printf()?\n",
-    "Q9: Which is the largest ocean on Earth?\n",
-    "Q10: What color do you get by mixing red and blue?\n"};
-char correctAnswer[][256] = {
-    "\nA. echo(\" Hello, World\");\nB.printf(\"Hello, World\");\nC.cout "
-    "<<\"Hello, World\";\nD.print(\"Hello, World\")\n",
+#define MAX_QUESTIONS 10
+
+struct Questions {
+  char questionText[200];
+  char questionOption[4][100];
+  char correctOption[50];
 };
 
 void gameInfo();
-void startTheGame(char arr);
 void clearScreen();
 
 int main() {
-    char answ;
+  char answer;
   clearScreen();
   gameInfo();
   printf("\nEnter your answer (A/B/C/D): ");
-  sscanf(" %c", &answ);
-  startTheGame(answ);
+  sscanf(" %c", &answer);
 
   return 0;
 }
@@ -37,18 +27,22 @@ void gameInfo() {
   printf("\n      ***  Welcome  ***\n");
   printf("\n*** Let's play Quiz Game! ***\n");
 }
-void startTheGame(char arr) {
-  for (int i = 0; i <= sizeof(question) / sizeof(question[0]); i++) {
-    printf("%s", question[i]);
-    if (strcmp(arr, correctAnswer) == 0)
-    {
-      printf("✅ Correct: B\n");
-    }else{
-        printf("❌Wrong answer!!!!");
-    }
-    
+
+void startGame(char answer, struct Questions quiz) {
+  for (int i = 0; i < MAX_QUESTIONS; i++) {
+    printf("Questions nr: %d", i + 1);
+    printf("%s", );
   }
 }
+
+void defineQuestions() {
+  struct Questions quiz[MAX_QUESTIONS] = {
+      {"Q1: What is the correct syntax to output \" Hello, World \" in C?",
+       {"A. echo(\" Hello, World \");", "B.printf(\"Hello, World\");",
+        "C.cout << \"Hello, World\";", "D.print(\"Hello, World\") "},
+       "✅ Correct: B  -  printf(\" Hello, World \");"}};
+}
+
 void clearScreen() {
 #ifdef _WIN32
   system("cls");
