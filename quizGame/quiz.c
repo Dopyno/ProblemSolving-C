22e8ce18624100c8724a0ab79a218337f7560f0d
Dopyno <dopyno@yahoo.com>
2025-07-12 12:17:53 +0100

    quiz game

---
+++ quizGame/quiz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void gameInfo();
+
+int main() {
+
+  gameInfo();
+  
+  return 0;
+}
+
+void gameInfo() {
+  printf("\n      ***  Welcome  ***\n");
+  printf("\n*** Let's play Quiz Game! ***\n");
+}
