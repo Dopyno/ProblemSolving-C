56de847d6a09ab29aa49a9292fe364d8196b3ce0
Dopyno <dopyno@yahoo.com>
2025-08-30 12:59:02 +0100

    up

--- First week/week_10.c
+++ First week/week_10.c
@@ -4,10 +4,43 @@ float calculateInterest(float principal, float rate, float time) {
   return (principal * rate * time) / 100;
 }
 
+int findMax(int a, int b, int c){
+    if(a >= b && a >= c){
+        return a;
+    }
+    else if(b >= a && b >= c){
+        return b;
+    }else{
+        return c;
+    }
+}
+int findMin(int a, int b, int c) {
+  if (a <= b && a <= c) {
+    return a;
+  } else if (b <= a && b <= c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+void sumArr(int arr[], int length){
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+      sum += arr[i];
+    }
+    printf("The total is: %d\n", sum);
+}
+
 int main(){
-
-
-    float nr = calculateInterest(200,1.5, 2);
-    printf("%.2f", nr);
+    // int a, b, c;
+    // printf("Please enter three numbers to find the max value: \n");
+    // scanf("%d %d %d", &a, &b, &c);
+    // printf("The max value is %d\n", findMax(a, b, c));
+    // printf("The min value is %d\n", findMin(a, b, c));
+    int arrayModel[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arrayModel)/sizeof(arrayModel[0]);
+    sumArr(arrayModel, size);
+    
     return 0;
 }
