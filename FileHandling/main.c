510b1918f9b74ca8393ae6e148f7db3133b10065
Dopyno <dopyno@yahoo.com>
2025-08-31 13:24:10 +0100

    up

--- FileHandling/main.c
+++ FileHandling/main.c
@@ -1,7 +1,72 @@
-#include<stdio.h>
+// #include <stdio.h>
 
-int main(){
+// int createFile() {
+//   FILE *fileAccess;
+//   fileAccess = fopen("welcome.txt", "w");
 
+//   if (fileAccess == NULL) {
+//     printf("Error: Could not create the file!");
+//     return 1;
+//   } else {
+//     if (fprintf(fileAccess, "This file has been accessed!") < 0) {
+//       printf("Error: Could not write to file!");
+//       return 0;
+//     } else {
+//       printf("Success: your data has bees written!");
+//       return 0;
+//     }
+//     fclose(fileAccess);
+//   }
+// }
 
-    return 0;
+// int main() {
+//   createFile();
+//   return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//   FILE *fptr = fopen("logfile.txt", "a");
+
+//   if (fptr == NULL) {
+//     printf("Error: Could not open file!\n");
+//     return 1;
+//   } else if (fprintf(fptr, "Your files have been scanned.\n") < 0) {
+//     printf("Error: Could not write first message!\n");
+//     fclose(fptr);
+//     return 1;
+//   } else if (fprintf(fptr, "No viruses have been found.\n") < 0) {
+//     printf("Error: Could not write second message!\n");
+//     fclose(fptr);
+//     return 1;
+//   } else {
+//     fclose(fptr);
+//     printf("Data appended successfully.\n");
+//   }
+
+//   return 0;
+// }
+#include <stdio.h>
+
+int main() {
+  FILE *fptr = fopen("logfile.txt", "a");
+
+  if (fptr == NULL) {
+    printf("Error: Could not open file!\n");
+    return 1;
+  } else if (fprintf(fptr, "Your files have been scanned.\n") < 0) {
+    printf("Error: Could not write first message!\n");
+    fclose(fptr);
+    return 1;
+  } else if (fprintf(fptr, "No viruses have been found.\n") < 0) {
+    printf("Error: Could not write second message!\n");
+    fclose(fptr);
+    return 1;
+  } else {
+    fclose(fptr);
+    printf("Data appended successfully.\n");
+  }
+
+  return 0;
 }
